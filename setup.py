bfd641a1715478f803bf0ec03c1ab1fb2e9722ea
dtzxyangq <dtzxyangq@foxmail.com>
2021-08-06 12:08:24 +0800

    commit the whole repo

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# version: 1.0
+# author: Qian Yang
+# Contact: dtzxyangq@foxmail.com
+
+import os,sys,io
+from distutils import setup
+from setuptools import find_packages
+
+VERSION = '1.0'
+
+tests_require= []
+install_requires = []
+
+setup(name='HiSTra',
+     version=VERSION,
+     description=,)
